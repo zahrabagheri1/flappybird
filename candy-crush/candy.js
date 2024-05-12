09d199ed043c604f9e216514d24d340daf3e0c3b
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-05-12 17:43:55 +0330

    Drag Functionality 🍭

--- candy-crush/candy.js
+++ candy-crush/candy.js
@@ -5,6 +5,9 @@ let rows = 9;
 let columns = 9;
 let score = 0;
 
+var currTile;
+var otherTile;
+
 window.onload = function () {
   startGame();
 };
@@ -21,7 +24,15 @@ function startGame() {
       let tile = document.createElement("img");
       tile.id = r.toString() + "-" + c.toString();
       tile.src = "./images/" + randomCandy() + ".png";
-      
+
+      // * Drag Functionality
+      tile.addEventListener("dragstart", dragStart); //? Click on a candy, initialize drag process
+      tile.addEventListener("dragover", dragOver); //? Clicking on candy, moving mouse to drag the candy
+      tile.addEventListener("dragenter", dragEnter); //? Dragging candy onto another candy
+      tile.addEventListener("dragleave", dragLeave); //? Leave candy over another candy
+      tile.addEventListener("drop", dragDrop); //? Dropping a candy over another candy
+      tile.addEventListener("dragend", dragEnd); //? After drag process completed, we swap candies
+
       document.getElementById("board").append(tile);
       row.push(tile);
     }
@@ -29,3 +40,33 @@ function startGame() {
   }
   console.log(board);
 }
+
+function dragStart() {
+  // ! This refers to tile that was dragging
+  currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {}
+
+function dragDrop() {
+    // ! This refers to the target tile that was dropping on
+    otherTile = this;
+}
+
+
+function dragEnd() {
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+}
+
